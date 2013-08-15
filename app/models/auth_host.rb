b329ac5cad551624863da767d1181a374c65b647
Samuel Sayer <ssayer@mitre.org>
2013-08-15 10:59:56 -0400

    make authorization path configurable

--- app/models/auth_host.rb
+++ app/models/auth_host.rb
@@ -11,12 +11,10 @@ class AuthHost
     query = {'client_id' => @config.client_id, 'client_secret' => @config.client_secret,
              'response_type' => 'code', 'redirect_uri' => redirect_uri,
              'scope' => "vitals"}.to_query
-    path = '/oauth/authorization'
+    path = @config.auth_code_path
 
-    if use_https
-      URI::HTTPS.build(host: @config.host, path: path, query: query, port: @config.port)      
-    else
-      URI::HTTP.build(host: @config.host, path: path, query: query, port: @config.port)
-    end
+    mode = use_https ? URI::HTTPS : URI::HTTP
+
+    mode.build(host: @config.host, path: path, query: query, port: @config.port)      
   end
 end
